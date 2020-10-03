82616325338f9dfe5cb5bc68a9e31ea7c40c5d53
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-03 22:15:27 +0530

    Added : First Name Validation Code

--- pattern2.sh
+++ pattern2.sh
@@ -2,4 +2,19 @@
 
 echo "Welcome to Regex"
 
+First_Name="^([[:upper:]]){3}[[:alpha:]]*$"
 
+echo "Enter your first name"
+
+read first_name
+
+if [[ $first_name =~ $First_Name ]]
+then
+
+echo "Valid"
+
+else
+
+echo "Invalid"
+
+fi

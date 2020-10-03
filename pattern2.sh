4bc7799b41915661e61a1e49dc485e95293fd2a6
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-03 22:39:33 +0530

    Added : Last Name Validation Code

--- pattern2.sh
+++ pattern2.sh
@@ -4,6 +4,8 @@ echo "Welcome to Regex"
 
 First_Name="^([[:upper:]]){3}[[:alpha:]]*$"
 
+Last_Name="^([[:upper:]]){3}[[:alpha:]]*$"
+
 echo "Enter your first name"
 
 read first_name
@@ -18,3 +20,19 @@ else
 echo "Invalid"
 
 fi
+
+echo "Enter your last name"
+
+read last_name
+
+if [[ $last_name =~ $Last_Name ]]
+then
+
+echo "Valid"
+
+else
+
+echo "Invalid"
+
+fi
+
